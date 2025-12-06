a57ddf8dbbaea34593e48fbf9f63acfbd74aa702
Joydeepbarik <joydeepbarik007@gmail.com>
2025-12-07 04:29:11 +0530

    style: Student Information

--- src/Pages/Academics/StudentInformation.jsx
+++ src/Pages/Academics/StudentInformation.jsx
@@ -1,6 +1,33 @@
+import { StudentInformationWrapper } from "../../Styles/AcademicStyle";
+
 const StudentInformationPage = () => {
-    return(
-        <></>
+    return (
+        <>
+            <StudentInformationWrapper>
+                <div className="page_head">
+                    <h2>Student Directory</h2>
+                </div>
+                <div className="student_search_sec">
+                    <div className="search_sec">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Search by Mobile Number" />
+                    </div>
+                    <div className="filter_sec">
+                        <div className="filter_btn">
+                            <p>Select Class</p>
+                            <i className="fa-solid fa-angle-down"></i>
+                        </div>
+                        <div className="filter_dropdown"></div>
+                    </div>
+                    <div className="add_btn">
+                        <button>
+                            <i className="fa-solid fa-plus"></i>
+                            <p>Create Employee</p>
+                        </button>
+                    </div>
+                </div>
+            </StudentInformationWrapper>
+        </>
     );
 }
 
--- src/Styles/AcademicStyle.jsx
+++ src/Styles/AcademicStyle.jsx
@@ -4,4 +4,119 @@ const colors = colorNames();
 
 export const StudentInformationWrapper = styled('div')`
     position: relative;
+    width: 100%;
+    padding: 0 15px;
+    display: flex;
+    flex-direction: column;
+
+    .page_head {
+        position: relative;
+        width: 100%;
+        display: flex;
+
+        h2 {
+            position: relative;
+            font-size: 19px;
+            font-weight: 600;
+            color: ${colors.customColors.blackColor};
+        }
+    }
+
+    .student_search_sec {
+        position: relative;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+
+        .search_sec {
+            position: relative;
+            height: 35px;
+            padding: 5px 0;
+            display: flex;
+            border: 1px solid ${colors.customColors.borderColor};
+            border-radius: 6px;
+
+            i {
+                position: relative;
+                width: 40px;
+                height: 100%;
+                font-size: 13px;
+                color: ${colors.customColors.blackColor3};
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-right: 1px solid ${colors.customColors.borderColor};
+            }
+
+            input {
+                position: relative;
+                width: 300px;
+                height: 100%;
+                border: none;
+                outline: none;
+                padding: 0 15px;
+                font-size: 12px;
+                color: ${colors.customColors.blackColor1};
+            }
+        }
+
+        .filter_sec {
+            position: relative;
+            margin-left: 20px;
+
+            .filter_btn {
+                position: relative;
+                width: 150px;
+                height: 35px;
+                border: 1px solid ${colors.customColors.borderColor};
+                border-radius: 6px;
+                display: flex;
+                align-items: center;
+                padding: 5px 15px;
+                cursor: pointer;
+
+                p {
+                    position: relative;
+                    font-size: 12px;
+                    color: ${colors.customColors.blackColor1};
+                }
+
+                i {
+                    position: relative;
+                    margin-left: auto;
+                    color: ${colors.customColors.blackColor1};
+                    font-size: 13px;
+                }
+            }
+        }
+
+        .add_btn {
+            position: relative;
+            margin-left: auto;
+
+            button {
+                position: relative;
+                width: 200px;
+                height: 35px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                cursor: pointer;
+                border: 1px solid ${colors.customColors.greenColor};
+                background: ${colors.customColors.greenColor};
+                color: ${colors.customColors.whiteColor};
+                border-radius: 6px;
+                font-size: 13px;
+                font-weight: 500;
+
+                i {
+                    margin-right: 8px;
+                    font-size: 12px;
+                }
+            }
+        }
+    }
+
+    
 `;
