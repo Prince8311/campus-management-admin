e08e28698ab6d9a4e86f6a50056fc232d5dcc3b6
Joydeepbarik <joydeepbarik007@gmail.com>
2025-12-07 20:12:24 +0530

    style: fee transation

--- src/Pages/FinanceManagement/Screens/FeeTransactions.jsx
+++ src/Pages/FinanceManagement/Screens/FeeTransactions.jsx
@@ -1,6 +1,66 @@
+import { FeeTransactionsWrapper } from "../../../Styles/FinanceStyle";
+
 const FeeTransactionsPage = () => {
-    return(
-        <></>
+    return (
+        <>
+            <FeeTransactionsWrapper>
+                <div className="page_head">
+                    <h2>Fee Transation</h2>
+                </div>
+                <div className="fee_search_sec">
+                    <div className="search_sec">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Search by Amount" />
+                    </div>
+                    <div className="filter_sec">
+                        <div className="filter_btn">
+                            <p>Download</p>
+                            <i className="fa-solid fa-angle-down"></i>
+                        </div>
+                        <div className="filter_dropdown"></div>
+                    </div>
+                </div>
+                <div className="table_sec">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Student Name</th>
+                                <th>Class</th>
+                                <th>Amount</th>
+                                <th>Receipt No</th>
+                                <th>Paid On</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Joydeep Barik</td>
+                                <td>10 - A</td>
+                                <td>30,000</td>
+                                <td>SAS-2025-26-684</td>
+                                <td>7 Dec 2025</td>
+                                <td><p className="success">Success</p></td>
+                                <td>
+                                    <a className="view_btn"><i className="fa-solid fa-eye"></i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Joydeep Barik</td>
+                                <td>10 - A</td>
+                                <td>30,000</td>
+                                <td>SAS-2025-26-684</td>
+                                <td>7 Dec 2025</td>
+                                <td><p className="">Pending</p></td>
+                                <td>
+                                    <a className="view_btn"><i className="fa-solid fa-eye"></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </FeeTransactionsWrapper>
+        </>
     );
 }
 
--- src/Styles/FinanceStyle.jsx
+++ src/Styles/FinanceStyle.jsx
@@ -564,3 +564,277 @@ export const FeeCollectionWrapper = styled('div')`
         }
     }
 `;
+
+export const FeeTransactionsWrapper = styled('div')`
+    position: relative;
+    width: 100%;
+    padding: 0 15px;
+    display: flex;
+    flex-direction: column;
+
+    .page_head {
+        position: relative;
+        width: 100%;
+        display: flex;
+
+        h2 {
+            position: relative;
+            font-size: 21px;
+            font-weight: 600;
+            color: ${colors.customColors.blackColor};
+            font-family: "SUSE", sans-serif;
+        }
+    }
+
+    .fee_search_sec {
+        position: relative;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+
+        .search_sec {
+            position: relative;
+            height: 35px;
+            padding: 5px 0;
+            display: flex;
+            border: 1px solid ${colors.customColors.borderColor};
+            border-radius: 6px;
+
+            i {
+                position: relative;
+                width: 40px;
+                height: 100%;
+                font-size: 13px;
+                color: ${colors.customColors.blackColor3};
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-right: 1px solid ${colors.customColors.borderColor};
+            }
+
+            input {
+                position: relative;
+                width: 300px;
+                height: 100%;
+                border: none;
+                outline: none;
+                padding: 0 15px;
+                font-size: 12px;
+                color: ${colors.customColors.blackColor1};
+            }
+        }
+
+        .filter_sec {
+            position: relative;
+            margin-left: 20px;
+
+            .filter_btn {
+                position: relative;
+                width: 200px;
+                height: 35px;
+                border: 1px solid ${colors.customColors.borderColor};
+                border-radius: 6px;
+                display: flex;
+                align-items: center;
+                padding: 5px 15px;
+                cursor: pointer;
+
+                p {
+                    position: relative;
+                    font-size: 12px;
+                    color: ${colors.customColors.blackColor1};
+                }
+
+                i {
+                    position: relative;
+                    margin-left: auto;
+                    color: ${colors.customColors.blackColor1};
+                    font-size: 13px;
+                }
+            }
+        }
+    }
+
+    .table_sec {
+        position: relative;
+        width: 100%;
+        margin-top: 25px;
+
+        table {
+            position: relative;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+
+            thead {
+                position: relative;
+                width: 100%;
+                height: 40px;
+                background: ${colors.themeColor};
+                border-radius: 10px 10px 0px 0px;
+                border: 1px solid ${colors.themeColor};
+
+                tr {
+                    position: relative;
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                }
+
+                th {
+                    position: relative;
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    font-family: "Lemonada", cursive;
+                    font-size: 12px;
+                    word-break: break-all;
+                    color: ${colors.customColors.whiteColor};
+                    padding: 0 25px;
+                    font-weight: 600;
+
+                    &:nth-of-type(1) {
+                        width: 20%;
+                    }
+
+                    &:nth-of-type(2) {
+                        width: 12%;
+                        justify-content: center;
+                    }
+
+                    &:nth-of-type(3) {
+                        width: 12%;
+                        justify-content: center;
+                    }
+
+                    &:nth-of-type(4) {
+                        width: 17%;
+                        justify-content: center;
+                    }
+
+                    &:nth-of-type(5) {
+                        width: 17%;
+                        justify-content: center;
+                    }
+
+                    &:nth-of-type(6) {
+                        width: 12%;
+                        justify-content: center;
+                    }
+
+                    &:nth-of-type(7) {
+                        width: 10%;
+                        justify-content: center;
+                    }
+                }
+            }
+
+            tbody {
+                position: relative;
+                width: 100%;
+                border: 1px solid ${colors.customColors.whiteColor2};
+                border-top: none;
+                border-radius: 0px 0px 10px 10px;
+                display: flex;
+                flex-direction: column;
+                overflow: hidden;
+
+                tr {
+                    position: relative;
+                    width: 100%;
+                    min-height: 40px;
+                    display: flex;
+
+                    &:nth-of-type(even) {
+                        background: ${colors.customColors.blueColorLight};
+                    }
+
+                    td {
+                        position: relative;
+                        padding: 12px 25px;
+                        display: flex;
+                        color: ${colors.customColors.blackColor};
+                        font-size: 13px;
+                        word-break: break-all;
+                        line-height: 1.5;
+
+                        &:nth-of-type(1) {
+                            width: 20%;
+                        }
+
+                        &:nth-of-type(2) {
+                            width: 12%;
+                            justify-content: center;
+                        }
+
+                        &:nth-of-type(3) {
+                            width: 12%;
+                            justify-content: center;
+                        }
+
+                        &:nth-of-type(4) {
+                            width: 17%;
+                            justify-content: center;
+                        }
+
+                        &:nth-of-type(5) {
+                            width: 17%;
+                            justify-content: center;
+                        }
+
+                        &:nth-of-type(6) {
+                            width: 12%;
+                            justify-content: center;
+
+                            p {
+                                position: relative;
+                                font-size: 10px;
+                                font-weight: 500;
+                                line-height: 1;
+                                display: flex;
+                                align-items: center;
+                                border-radius: 20px;
+                                padding: 4px 15px;
+                                color: ${colors.customColors.redColor};
+                                background: ${colors.customColors.redColorLight};
+
+                                &.success {
+                                    color: ${colors.customColors.greenColor};
+                                    background: ${colors.customColors.greenColorLight};
+                                }
+                            }
+                        }
+
+                        &:nth-of-type(7) {
+                            width: 10%;
+                            justify-content: center;
+
+                            a {
+                                position: relative;
+                                text-decoration: none;
+                                cursor: pointer;
+                                font-size: 13px;
+
+                               &.view_btn {
+                                    color: ${colors.customColors.blueColor1};
+                               }
+                            }
+                        }
+
+                        &.empty_message {
+                            position: relative;
+                            width: 100%;
+                            padding: 12px 25px;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            font-size: 13px;
+                            color: ${colors.customColors.blackColor2};
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
