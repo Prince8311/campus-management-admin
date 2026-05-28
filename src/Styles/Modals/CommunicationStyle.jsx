85b986cf9124c4c93f0891cd2c504e87e0c79549
Joydeepbarik <joydeepbarik007@gmail.com>
2026-05-29 02:56:04 +0530

    feat: change mail list

---
+++ src/Styles/Modals/CommunicationStyle.jsx
@@ -0,0 +1,240 @@
+import styled from "@emotion/styled";
+import { colorNames } from "../../Theme/Colors";
+const colors = colorNames();
+
+export const MessageCreateWrapper = styled('div')`
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 40px 0;
+    z-index: 1000;
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: none;
+    padding-left: 265px;
+    transition: all 0.3s ease;
+    
+    &.active {
+        opacity: 1;
+        visibility: visible;
+        pointer-events: initial;
+        transition: all 0.3s ease;
+    }
+
+    .modal_box {
+        position: relative;
+        width: 550px;
+        max-height: 100%;
+        background: ${colors.customColors.whiteColor};
+        box-shadow: 10px 15px 20px ${colors.boxShadowColors.shadowColor1}, -5px -5px 10px ${colors.boxShadowColors.shadowColor2};
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        transform: translateY(-150px);
+        transition: transform 0.8s ease;
+        overflow-y: auto;
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+
+        &.active {
+            transform: translateY(0);
+            transition: transform 0.8s ease;
+        }
+
+        .modal_head {
+            position: relative;
+            width: 100%;
+            padding: 13px 20px;
+            border-bottom: 1px solid ${colors.customColors.borderColor};
+            display: flex;
+            align-items: center;
+
+            h4 {
+                position: relative;
+                max-width: calc(100% - 40px);
+                font-size: 14px;
+                line-height: 1;
+                font-weight: 600;
+                font-style: italic;
+                color: ${colors.customColors.blackColor1};
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+
+            .close_sec {
+                position: relative;
+                margin-left: auto;
+                width: 40px;
+                display: flex;
+                justify-content: flex-end;
+
+                a {
+                    position: relative;
+                    margin-left: auto;
+                    font-size: 15px;
+                    color: ${colors.customColors.blackColor1};
+                    cursor: pointer;
+                }
+            }
+        }
+
+        .modal_body {
+            position: relative;
+            width: 100%;
+            padding: 11px 20px;
+            display: flex;
+            flex-direction: column;
+
+            .body_inner {
+                position: relative;
+                width: 100%;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+
+                .input_box {
+                    position: relative;
+                    width: 100%;
+                    margin-bottom: 10px;
+
+                    input {
+                        position: relative;
+                        width: 100%;
+                        height: 37px;
+                        font-size: 12px;
+                        border-radius: 5px;
+                        padding: 5px 15px;
+                        outline: none;
+                        border: none;
+                        margin-top: 3px;
+                        background: ${colors.customColors.lightBackground3};
+                    }
+
+                    textarea {
+                        position: relative;
+                        width: 100%;
+                        height: 80px;
+                        font-size: 12px;
+                        border-radius: 5px;
+                        padding: 5px 15px;
+                        outline: none;
+                        border: none;
+                        margin-top: 3px;
+                        background: ${colors.customColors.lightBackground3};
+                        resize: none;
+                    }
+
+                    span {
+                        position: relative;
+                        display: flex;
+                        align-items: center;
+                        font-size: 12px;
+                        font-weight: 400;
+                        color: ${colors.customColors.blackColor2};
+
+                        p {
+                            color: ${colors.customColors.redColor};
+                            margin-left: 2px;
+                        }
+                    }
+                }
+            }
+        }
+
+        .modal_btn {
+            position: relative;
+            width: 100%;
+            padding: 13px 20px;
+            display: flex;
+            align-items: center;
+            border-top: 1px solid ${colors.customColors.borderColor};
+
+            p {
+                position: relative;
+                font-size: 12px;
+                color: ${colors.customColors.blackColor2};
+                margin-right: 8px;
+            }
+
+            .toggle_bar {
+                position: relative;
+                width: 50px;
+                display: flex;
+                justify-content: flex-start;
+
+                input[type="checkbox"] {
+                    display: none;
+                }
+
+                label {
+                    position: relative;
+                    width: 40px;
+                    height: 21px;
+                    background: ${colors.customColors.whiteColor2};
+                    border-radius: 25px;
+                    padding: 3px;
+                    display: flex;
+                    align-items: center;
+                    cursor: pointer;
+                    transition: all 0.5s ease;
+
+                    span {
+                        position: relative;
+                        width: 15px;
+                        height: 100%;
+                        background: ${colors.customColors.whiteColor};
+                        border-radius: 50%;
+                        transition: all 0.5s ease;
+                    }
+                }
+
+                input[type="checkbox"]:checked ~ label {
+                    background: ${colors.customColors.blueColor1};
+                    transition: all 0.5s ease;
+                }
+
+                input[type="checkbox"]:checked ~ label span {
+                    transform: translateX(19px);
+                    transition: all 0.5s ease;
+                }
+            }
+
+            button {
+                position: relative;
+                width: 160px;
+                height: 35px;
+                font-size: 13px;
+                font-weight: 500;
+                cursor: pointer;
+                border-radius: 6px;
+                overflow: hidden;
+                border: none;
+                background: ${colors.customColors.blueColor1};
+                color: ${colors.customColors.whiteColor};
+                margin-left: auto;
+                transition: all 0.5s ease;
+
+                &:hover {
+                    border-radius: 25px;
+                    transition: all 0.5s ease;
+                }
+
+                &:disabled {
+                    cursor: not-allowed;
+                    opacity: 0.4;
+                    transition: all 0.5s ease;
+                }
+            }
+        }
+    }
+`;
