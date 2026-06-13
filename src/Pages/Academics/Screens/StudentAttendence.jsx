ffa10f77d55bbb225c993fe4d3bb706aa29678dc
Prince8311 <sourishmondal.vizac@gmail.com>
2026-06-14 01:12:28 +0530

    feat: class attendance

--- src/Pages/Academics/Screens/StudentAttendence.jsx
+++ src/Pages/Academics/Screens/StudentAttendence.jsx
@@ -16,6 +16,7 @@ const StudentAttendencePage = () => {
     const [isCalendarOpen, setIsCalendarOpen] = useState(false);
     const [isConfigarationModalOpen, setIsConfigarationModalOpen] = useState(false);
     const [intialConfigarationsLoading, setIntialConfigarationsLoading] = useState(false);
+    const [configurationList, setConfigurationList] = useState([]);
 
     const getFormattedCurrentDate = () => {
         const today = new Date();
@@ -34,6 +35,7 @@ const StudentAttendencePage = () => {
         try {
             const response = await axiosInstance.get(api.attendanceConfigurationList);
             if (response?.data.status === 200) {
+                setConfigurationList(response?.data.data);
                 console.log("listttttttttttttt", response?.data);
             }
         } catch (error) {
@@ -60,159 +62,170 @@ const StudentAttendencePage = () => {
             <StudentAttendenceWrapper>
                 <div className="page_head">
                     <h2>Student Attendance</h2>
-                    <div className="configuration_btn">
-                        <button onClick={handleOpenConfigarationModal}>
-                            <i className="fa-solid fa-sliders"></i>
-                            <p>Attendence Configaration</p>
-                        </button>
-                    </div>
+                    {
+                        configurationList.length > 0 &&
+                        <div className="configuration_btn">
+                            <button onClick={handleOpenConfigarationModal}>
+                                <i className="fa-solid fa-sliders"></i>
+                                <p>Attendence Configaration</p>
+                            </button>
+                        </div>
+                    }
                 </div>
-                <div className="overview_section">
-                    <div className="sec_head">
-                        <h6>Overview Report</h6>
-                    </div>
-                    <div className="graph_sec">
-                        <div className="graph_first_box">
-                            <h4>Weekly Report <span>(10 May - 17 May)</span></h4>
-                            <div className="graph_box">
-                                <StudentWeeklyAttendanceGraph />
-                            </div>
-                        </div>
-                        <div className="graph_second_box">
-                            <h4>Daily Report <span>(10 May)</span></h4>
-                            <div className="graph_box">
-                                <StudentDailyAttendance />
-                            </div>
-                        </div>
-                        <div className="another_sec">
-                            <div className="sec_box">
-                                <p>Total Students</p>
-                                <span>1500</span>
-                            </div>
-                            <div className="sec_box">
-                                <p>Present Students</p>
-                                <span>1000</span>
-                            </div>
-                            <div className="sec_box">
-                                <p>Absent Students</p>
-                                <span>500</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="report_details_sec">
-                    <div className="report_head">
-                        <h6>Report Details</h6>
-                        <div className="heading_option_part">
-                            <div className="inner_part">
-                                <a className="part_box">
-                                    <span><i className="fa-solid fa-users"></i></span>
-                                    <h6>Class Wise</h6>
-                                </a>
-                                <a className="part_box active">
-                                    <span><i className="fa-regular fa-clock"></i></span>
-                                    <h6>Period Wise</h6>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="student_search_sec">
-                        <div className="search_sec">
-                            <i className="fa-solid fa-magnifying-glass"></i>
-                            <input type="text" placeholder="Search for class/section or class teacher" />
-                        </div>
-                        <div className="filter_sec">
-                            <div className="filter_btn" onClick={() => setIsCalendarOpen(prev => !prev)}>
-                                <i className="fa-solid fa-filter"></i>
-                                <p>{filterDate || "Filter by date"}</p>
-                            </div>
-                            {
-                                isCalendarOpen && (
-                                    <div className="dropdown">
-                                        <Calender setFinalSelectedDate={setFilterDate} />
+                {
+                    configurationList.length > 0 ? (
+                        <>
+                            <div className="overview_section">
+                                <div className="sec_head">
+                                    <h6>Overview Report</h6>
+                                </div>
+                                <div className="graph_sec">
+                                    <div className="graph_first_box">
+                                        <h4>Weekly Report <span>(10 May - 17 May)</span></h4>
+                                        <div className="graph_box">
+                                            <StudentWeeklyAttendanceGraph />
+                                        </div>
                                     </div>
-                                )
-                            }
-                        </div>
-                    </div>
-                    <div className="tab_sec">
-                        <div className="tab_inner">
-                            <li className="active">Primary</li>
-                            <li>Middle</li>
-                            <li>Secondary</li>
-                        </div>
-                    </div>
-                    <div className="table_sec">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Class & Section</th>
-                                    <th>Total Students</th>
-                                    <th>Present</th>
-                                    <th>Absent</th>
-                                    <th>Percentage</th>
-                                    <th>Mark by</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>10 - D</td>
-                                    <td>100</td>
-                                    <td><i className="fa-solid fa-minus"></i></td>
-                                    <td><i className="fa-solid fa-minus"></i></td>
-                                    <td>
-                                        <p className="">Not Marked</p>
-                                    </td>
-                                    <td>
-                                        <div className="left_table_sec">
-                                            <h5>JB</h5>
+                                    <div className="graph_second_box">
+                                        <h4>Daily Report <span>(10 May)</span></h4>
+                                        <div className="graph_box">
+                                            <StudentDailyAttendance />
                                         </div>
-                                        <div className="right_table_sec">
-                                            <h6>Joydeep Barik</h6>
-                                            <p>9749708386</p>
+                                    </div>
+                                    <div className="another_sec">
+                                        <div className="sec_box">
+                                            <p>Total Students</p>
+                                            <span>1500</span>
                                         </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="secondtable_sec">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Class & Section</th>
-                                    <th>Period</th>
-                                    <th>Total Students</th>
-                                    <th>Present</th>
-                                    <th>Absent</th>
-                                    <th>Percentage</th>
-                                    <th>Mark by</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>10 - D</td>
-                                    <td>First <span>(10:00 am - 11:45 am)</span></td>
-                                    <td>100</td>
-                                    <td><i className="fa-solid fa-minus"></i></td>
-                                    <td><i className="fa-solid fa-minus"></i></td>
-                                    <td>
-                                        <p className="">Not Marked</p>
-                                    </td>
-                                    <td>
-                                        <div className="left_table_sec">
-                                            <h5>JB</h5>
+                                        <div className="sec_box">
+                                            <p>Present Students</p>
+                                            <span>1000</span>
                                         </div>
-                                        <div className="right_table_sec">
-                                            <h6>Joydeep Barik</h6>
-                                            <p>9749708386</p>
+                                        <div className="sec_box">
+                                            <p>Absent Students</p>
+                                            <span>500</span>
                                         </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="report_details_sec">
+                                <div className="report_head">
+                                    <h6>Report Details</h6>
+                                    <div className="heading_option_part">
+                                        <div className="inner_part">
+                                            <a className="part_box">
+                                                <span><i className="fa-solid fa-users"></i></span>
+                                                <h6>Class Wise</h6>
+                                            </a>
+                                            <a className="part_box active">
+                                                <span><i className="fa-regular fa-clock"></i></span>
+                                                <h6>Period Wise</h6>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="student_search_sec">
+                                    <div className="search_sec">
+                                        <i className="fa-solid fa-magnifying-glass"></i>
+                                        <input type="text" placeholder="Search for class/section or class teacher" />
+                                    </div>
+                                    <div className="filter_sec">
+                                        <div className="filter_btn" onClick={() => setIsCalendarOpen(prev => !prev)}>
+                                            <i className="fa-solid fa-filter"></i>
+                                            <p>{filterDate || "Filter by date"}</p>
+                                        </div>
+                                        {
+                                            isCalendarOpen && (
+                                                <div className="dropdown">
+                                                    <Calender setFinalSelectedDate={setFilterDate} />
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                                <div className="tab_sec">
+                                    <div className="tab_inner">
+                                        <li className="active">Primary</li>
+                                        <li>Middle</li>
+                                        <li>Secondary</li>
+                                    </div>
+                                </div>
+                                <div className="table_sec">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Class & Section</th>
+                                                <th>Total Students</th>
+                                                <th>Present</th>
+                                                <th>Absent</th>
+                                                <th>Percentage</th>
+                                                <th>Mark by</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>10 - D</td>
+                                                <td>100</td>
+                                                <td><i className="fa-solid fa-minus"></i></td>
+                                                <td><i className="fa-solid fa-minus"></i></td>
+                                                <td>
+                                                    <p className="">Not Marked</p>
+                                                </td>
+                                                <td>
+                                                    <div className="left_table_sec">
+                                                        <h5>JB</h5>
+                                                    </div>
+                                                    <div className="right_table_sec">
+                                                        <h6>Joydeep Barik</h6>
+                                                        <p>9749708386</p>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="secondtable_sec">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Class & Section</th>
+                                                <th>Period</th>
+                                                <th>Total Students</th>
+                                                <th>Present</th>
+                                                <th>Absent</th>
+                                                <th>Percentage</th>
+                                                <th>Mark by</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>10 - D</td>
+                                                <td>First <span>(10:00 am - 11:45 am)</span></td>
+                                                <td>100</td>
+                                                <td><i className="fa-solid fa-minus"></i></td>
+                                                <td><i className="fa-solid fa-minus"></i></td>
+                                                <td>
+                                                    <p className="">Not Marked</p>
+                                                </td>
+                                                <td>
+                                                    <div className="left_table_sec">
+                                                        <h5>JB</h5>
+                                                    </div>
+                                                    <div className="right_table_sec">
+                                                        <h6>Joydeep Barik</h6>
+                                                        <p>9749708386</p>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="empty_screen"></div>
+                    )
+                }
                 <AttendenceModal
                     isAttendenceModalOpen={isAttendenceModalOpen}
                     setIsAttendenceModalOpen={setIsAttendenceModalOpen}
